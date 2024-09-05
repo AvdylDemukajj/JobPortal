34e6093c1df81d03c48e64be77085e8e1c22ff27
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 16:59:20 +0200

    "Define single job route'

--- backend/routes/jobsRoutes.js
+++ backend/routes/jobsRoutes.js
@@ -6,3 +6,6 @@ const { isAuthenticated, isAdmin } = require('../middleware/auth');
 // Jobs routes
 // /api/job/create
 router.post('/job/create', isAuthenticated, isAdmin, createJob);
+// /api/job/id
+router.get('/job/:id', singleJob);
+
