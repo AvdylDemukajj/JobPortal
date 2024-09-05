dd8c0bd352196a0c284e2dd8b5e922b709a5286b
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 17:12:37 +0200

    "Set up static files and error handling"

--- backend/app.js
+++ backend/app.js
@@ -53,4 +53,27 @@ app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', jobTypeRoute);
 app.use('/api', jobRoute);
-// ...
+
+__dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+// Error middleware
+app.use(errorHandler);
+
+// Port
+const port = process.env.PORT || 9000
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
