d5f63dc525f478afed35e2b39b2fd877169adcf3
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 16:55:36 +0200

    "Define signup route"

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { signup, signin, logout, userProfile } = require('../controllers/authController');
 const { isAuthenticated } = require('../middleware/auth');
+
+// Auth routes
+// /api/signup
+router.post('/signup', signup);
+
