8fdefbfd0a22435f5f1ec2f5e60d0c3ee7aeaa19
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 16:59:02 +0200

    "Define create job route"

--- backend/routes/jobsRoutes.js
+++ backend/routes/jobsRoutes.js
@@ -2,3 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { createJob, singleJob, updateJob, showJobs, deleteJob } = require('../controllers/jobsController');
 const { isAuthenticated, isAdmin } = require('../middleware/auth');
+
+// Jobs routes
+// /api/job/create
+router.post('/job/create', isAuthenticated, isAdmin, createJob);
