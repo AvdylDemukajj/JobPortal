9af2a45e877549437b1220b9798661d3e9ea5014
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 17:11:46 +0200

    " Set up database connection"

--- backend/app.js
+++ backend/app.js
@@ -14,3 +14,15 @@ const userRoutes = require('./routes/userRoutes');
 const jobTypeRoute = require('./routes/jobsTypeRoutes');
 const jobRoute = require('./routes/jobsRoutes');
 
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
+// Database connection
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
