2830e1448dc44125d95ae39c6c6ca9793ed44143
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 16:55:20 +0200

    "Import authentication middleware"

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,3 +1,4 @@
 const express = require('express');
 const router = express.Router();
 const { signup, signin, logout, userProfile } = require('../controllers/authController');
+const { isAuthenticated } = require('../middleware/auth');
