9ee39fba987d475c4f066696962b779ccfe4b696
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 17:07:36 +0200

    "Define all users route"

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { allUsers, singleUser, editUser, deleteUser, createUserJobsHistory } = require('../controllers/userController');
 const { isAuthenticated, isAdmin } = require('../middleware/auth');
+
+// User routes
+// /api/allusers
+router.get('/allusers', isAuthenticated, isAdmin, allUsers);
+
