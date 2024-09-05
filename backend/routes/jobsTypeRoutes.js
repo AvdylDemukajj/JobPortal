f39d81d8beac530d1fd7291263d54ae114c29261
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 17:02:47 +0200

    "Import authentication middleware"

--- backend/routes/jobsTypeRoutes.js
+++ backend/routes/jobsTypeRoutes.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const router = express.Router();
 const { createJobType, allJobsType, updateJobType, deleteJobType } = require('../controllers/jobsTypeController');
-
+const { isAuthenticated, isAdmin } = require('../middleware/auth');
