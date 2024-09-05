a893f785f07dc208962685621a0e04d264594f5b
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 16:35:53 +0200

    "signin function"

---
+++ backend/controllers/authController.js
@@ -0,0 +1,25 @@
+
+const User = require('../models/userModel');
+const ErrorResponse = require('../utils/errorResponse');
+
+
+exports.signup = async (req, res, next) => {
+    const { email } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+        return next(new ErrorResponse("E-mail already registred", 400));
+    }
+    try {
+        req.body.role = 0; // this is to prevent anyone creating an admin user.
+        const user = await User.create(req.body);
+        res.status(201).json({
+            success: true,
+            user
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+
