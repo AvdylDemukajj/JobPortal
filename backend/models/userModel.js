29eac6932fa0b46edd57438de1116353288145e8
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 16:27:26 +0200

    "creating user model"

---
+++ backend/models/userModel.js
@@ -0,0 +1,50 @@
+
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+const userSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        trim: true,
+        required: [true, 'first name is required'],
+        maxlength: 32,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: [true, 'last name is required'],
+        maxlength: 32,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'e-mail is required'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: [true, 'password is required'],
+        minlength: [6, 'password must have at least (6) caracters'],
+    },
+
+    jobsHistory: [jobsHistorySchema],
+
+    role: {
+        type: Number,
+        default: 0
+    }
+
+}, { timestamps: true })
+
+
+
+module.exports = mongoose.model("User", userSchema);
