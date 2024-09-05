129316e0a101d3ff3a83464ec0fd1fb1a31de319
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 16:30:44 +0200

    "adding job history schema"

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,44 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 
+const jobsHistorySchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        trim: true,
+        maxlength: 70,
+    },
+
+    description: {
+        type: String,
+        trim: true
+    },
+    salary: {
+        type: String,
+        trim: true,
+    },
+    location: {
+        type: String,
+    },
+    interviewDate: {
+        type: Date,
+    },
+    applicationStatus: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected'],
+        default: 'pending'
+    },
+
+    user: {
+        type: ObjectId,
+        ref: "User",
+        required: true
+    },
+
+
+
+}, { timestamps: true })
+
 const userSchema = new mongoose.Schema({
 
     firstName: {
