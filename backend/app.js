701f286b6f2be72613af661f9c2897517b7320f1
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 17:10:29 +0200

    "Import dependencies and configuration"

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+var cors = require('cors');
+const path = require('path');
+const helmet = require('helmet');
 // ...
