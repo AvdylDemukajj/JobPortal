7b6f1d994f058d3ee39ac265dcc1b93aa7894f2d
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 16:56:32 +0200

    "Define user profile route"

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -10,3 +10,7 @@ router.post('/signup', signup);
 router.post('/signin', signin);
 // /api/logout
 router.get('/logout', logout);
+// /api/me
+router.get('/me', isAuthenticated, userProfile);
+
+module.exports = router;
