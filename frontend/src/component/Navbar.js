a154aa448f3c53d6c2b9ed529c35424eeb8621e0
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-06 20:38:08 +0200

    "Add navigation links (Home, Log In)"

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -4,6 +4,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 const Navbar = () => {
     return (
@@ -15,6 +17,10 @@ const Navbar = () => {
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     JOB PORTAL
                 </Typography>
+                <Box sx={{ display: 'flex' }}>
+                    <Button color="inherit">Home</Button>
+                    <Button color="inherit">Log In</Button>
+                </Box>
             </Toolbar>
         </AppBar>
     );
