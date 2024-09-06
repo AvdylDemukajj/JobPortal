a41eed9ff8a7df7e7fa46fb8ebd479be6b323fe3
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-06 21:41:09 +0200

    "adding load job type reducer "

--- frontend/src/redux/reducers/jobTypeReducer.js
+++ frontend/src/redux/reducers/jobTypeReducer.js
@@ -8,3 +8,25 @@ import {
     JOB_TYPE_LOAD_RESET,
     JOB_TYPE_LOAD_SUCCESS
 } from "../constants/jobTypeConstant"
+
+// load job type reducer
+export const loadJobTypeReducer = (state = { jobType: [] }, action) => {
+    switch (action.type) {
+        case JOB_TYPE_LOAD_REQUEST:
+            return { loading: true }
+        case JOB_TYPE_LOAD_SUCCESS:
+            return {
+                loading: false,
+                jobType: action.payload.jobT
+            }
+        case JOB_TYPE_LOAD_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case JOB_TYPE_LOAD_RESET:
+            return {}
+        default:
+            return state;
+    }
+}
