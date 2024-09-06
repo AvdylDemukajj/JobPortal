c8e1e54a39c0cb60a6506ccd9e3f3c7da58ce6bd
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-06 20:38:27 +0200

    "Add responsive mobile menu functionality"

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -6,24 +6,68 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/Menu';
+
+const pages = ['Home', 'Log In'];
 
 const Navbar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return (
         <AppBar position="static">
             <Toolbar>
-                <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                    onClick={handleOpenNavMenu}
+                >
                     <MenuIcon />
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     JOB PORTAL
                 </Typography>
-                <Box sx={{ display: 'flex' }}>
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">Log In</Button>
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                    {pages.map((page) => (
+                        <Button key={page} color="inherit">{page}</Button>
+                    ))}
                 </Box>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{ display: { xs: 'block', md: 'none' } }}
+                >
+                    {pages.map((page) => (
+                        <MenuItem key={page} onClick={handleCloseNavMenu}>
+                            <Typography textAlign="center">{page}</Typography>
+                        </MenuItem>
+                    ))}
+                </Menu>
             </Toolbar>
         </AppBar>
     );
-}
+};
 
 export default Navbar;
