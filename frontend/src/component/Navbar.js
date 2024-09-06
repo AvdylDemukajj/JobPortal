5bf134587e615ec42874c243f92e88242ba59278
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-06 20:37:50 +0200

    " Add MenuIcon for mobile navigation"

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
     return (
         <AppBar position="static">
             <Toolbar>
+                <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     JOB PORTAL
                 </Typography>
